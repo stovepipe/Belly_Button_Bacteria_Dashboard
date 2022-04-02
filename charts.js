38efe63fd4003953e2c8754f487326a26354c6f4
stovepipe <zach.mehlenbacher@gmail.com>
2022-04-02 18:10:47 -0400

    Formatting annotations complete

--- charts.js
+++ charts.js
@@ -104,7 +104,7 @@ function buildCharts(sample) {
         xanchor: 'center',
         y: -0.25,
         yanchor: 'center',
-        text: 'The bar chart displays up to 10 of the bacterial species<br>with the highest occurrences in the sample',
+        text: 'The bar chart displays up to 10 of the bacterial species<br>with the most occurrences in the sample',
         showarrow: false
       }]
     };
